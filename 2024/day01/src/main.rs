19dd5fb2decd83c5f354dddd88c3fe4f829c8716
Mohammed Al-Tameemi <Altameemi@pm.me>
2024-12-18 14:39:40 -0700

    new day 1 solution

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -1,71 +1,64 @@
-use std::collections::HashMap;
+fn convert_str_to_num(c: &u8) -> usize {
+    (c - '0' as u8) as usize
+}
 
 fn part1() {
-    let data = std::fs::read_to_string("./input.txt").unwrap();
-    let mut total: u64 = 0;
-    let mut path1: Vec<u32>;
-    let mut path2: Vec<u32>;
+    let data = include_bytes!("../input.txt");
+    let mut path1: Vec<usize>;
+    let mut path2: Vec<usize>;
 
     (path1, path2) = data
-        .lines()
+        .split(|&b| b == b'\n')
+        .filter(|line| !line.is_empty())
         .map(|line| {
-            let mut line_data = line.split("   ");
             (
-                line_data.next().unwrap().parse::<u32>().unwrap(),
-                line_data.next().unwrap().parse::<u32>().unwrap(),
+                line[..5].iter().map(convert_str_to_num).enumerate().fold(0, |acc, (index, number)| {
+                    acc + number * 10_u32.pow(4 - index as u32) as usize
+                }),
+                line[8..].iter().map(convert_str_to_num).enumerate().fold(0, |acc, (index, number)| {
+                    acc + number * 10_u32.pow(4 - index as u32) as usize
+                })
+            
             )
         })
         .unzip();
 
-    path1.sort();
-    path2.sort();
+    path1.sort_unstable();
+    path2.sort_unstable();
 
-    for i in 0..path1.len() {
-        total += (path1[i].abs_diff(path2[i])) as u64;
-    }
-
-    println!("{total}");
+    println!("{}", path1.iter().zip(path2).map(|(a, b)| a.abs_diff(b)).sum::<usize>());
 }
 
 fn part2() {
     let data = std::fs::read_to_string("./input.txt").unwrap();
-    let mut total: u64 = 0;
-    let mut path1: Vec<u32>;
-    let mut path2: Vec<u32>;
-    let mut path2_map: HashMap<u32, u32> = HashMap::new();
+    let mut path1: Vec<usize> = Vec::new();
+    let mut path2 = [0; 100_000];
 
-    (path1, path2) = data
-        .lines()
-        .map(|line| {
-            let mut line_data = line.split("   ");
-            (
-                line_data.next().unwrap().parse::<u32>().unwrap(),
-                line_data.next().unwrap().parse::<u32>().unwrap(),
-            )
-        })
-        .unzip();
-
-    path1.sort();
-    path2.sort();
-
-    for element in path2 {
-        if !path2_map.contains_key(&element) {
-            path2_map.insert(element, 0);
-        }
-
-        (*path2_map.get_mut(&element).unwrap()) += 1;
+    for line in data.lines() {
+        let mut line_data = line.split("   ");
+        path1.push(line_data.next().unwrap().parse::<usize>().unwrap());
+        path2[line_data.next().unwrap().parse::<usize>().unwrap()] += 1;
     }
 
-    for element in path1 {
-        if path2_map.contains_key(&element) {
-            total += (element * path2_map[&element]) as u64;
-        }
-    }
-
-    println!("{total}");
+    println!("{}", path1.into_iter().map(|n| path2[n] * n).sum::<usize>());
 }
 
 fn main() {
-    part1();
-    part2();
+    use std::time::Instant;
+    let now = Instant::now();
+
+    {
+        part1();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.5?}", elapsed);
+    let now = Instant::now();
+
+    {
+        part2();
+    }
+
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.5?}", elapsed);
 }
