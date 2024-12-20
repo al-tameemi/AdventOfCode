06439d46e7d37ec2ced61a983c2e519a9654c6a0
Mohammed Al-Tameemi <Altameemi@pm.me>
2024-12-20 04:17:45 -0700

    removed dep, optimized part 2

--- 2024/day01/src/main.rs
+++ 2024/day01/src/main.rs
@@ -2,7 +2,7 @@ fn convert_str_to_num(c: &u8) -> usize {
     (c - '0' as u8) as usize
 }
 
-fn part1() {
+fn part1() -> usize {
     let data = include_bytes!("../input.txt");
     let mut path1: Vec<usize>;
     let mut path2: Vec<usize>;
@@ -12,13 +12,20 @@ fn part1() {
         .filter(|line| !line.is_empty())
         .map(|line| {
             (
-                line[..5].iter().map(convert_str_to_num).enumerate().fold(0, |acc, (index, number)| {
-                    acc + number * 10_u32.pow(4 - index as u32) as usize
-                }),
-                line[8..].iter().map(convert_str_to_num).enumerate().fold(0, |acc, (index, number)| {
-                    acc + number * 10_u32.pow(4 - index as u32) as usize
-                })
-            
+                line[..5]
+                    .iter()
+                    .map(convert_str_to_num)
+                    .enumerate()
+                    .fold(0, |acc, (index, number)| {
+                        acc + number * 10_u32.pow(4 - index as u32) as usize
+                    }),
+                line[8..]
+                    .iter()
+                    .map(convert_str_to_num)
+                    .enumerate()
+                    .fold(0, |acc, (index, number)| {
+                        acc + number * 10_u32.pow(4 - index as u32) as usize
+                    }),
             )
         })
         .unzip();
@@ -26,39 +33,50 @@ fn part1() {
     path1.sort_unstable();
     path2.sort_unstable();
 
-    println!("{}", path1.iter().zip(path2).map(|(a, b)| a.abs_diff(b)).sum::<usize>());
+    path1
+        .iter()
+        .zip(path2)
+        .map(|(a, b)| a.abs_diff(b))
+        .sum::<usize>()
 }
 
-fn part2() {
-    let data = std::fs::read_to_string("./input.txt").unwrap();
+fn part2() -> usize {
+    let data = include_bytes!("../input.txt");
     let mut path1: Vec<usize> = Vec::new();
     let mut path2 = [0; 100_000];
 
-    for line in data.lines() {
-        let mut line_data = line.split("   ");
-        path1.push(line_data.next().unwrap().parse::<usize>().unwrap());
-        path2[line_data.next().unwrap().parse::<usize>().unwrap()] += 1;
+    for line in data.split(|&b| b == b'\n') {
+        path1.push(
+            line[..5]
+                .iter()
+                .map(convert_str_to_num)
+                .enumerate()
+                .fold(0, |acc, (index, number)| {
+                    acc + number * 10_u32.pow(4 - index as u32) as usize
+                }),
+        );
+        path2[line[8..]
+            .iter()
+            .map(convert_str_to_num)
+            .enumerate()
+            .fold(0, |acc, (index, number)| {
+                acc + number * 10_u32.pow(4 - index as u32) as usize
+            })] += 1;
     }
 
-    println!("{}", path1.into_iter().map(|n| path2[n] * n).sum::<usize>());
+    path1.into_iter().map(|n| path2[n] * n).sum::<usize>()
 }
 
 fn main() {
     use std::time::Instant;
     let now = Instant::now();
-
-    {
-        part1();
-    }
-
+    let value = part1();
     let elapsed = now.elapsed();
+    println!("Part 1: {value}");
     println!("Elapsed: {:.5?}", elapsed);
     let now = Instant::now();
-
-    {
-        part2();
-    }
-
+    let value = part2();
     let elapsed = now.elapsed();
+    println!("Part 2: {value}");
     println!("Elapsed: {:.5?}", elapsed);
 }
